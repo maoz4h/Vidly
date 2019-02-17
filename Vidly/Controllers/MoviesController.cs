035998b57310d4dcc8382706707a112969924a85
maozh <maoz4h@gmail.com>
2019-02-17 12:56:32 +0200

    Exercise - Display the list of customers and movies.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,26 +12,20 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer() {Name = "Customer 1"},
-                new Customer() {Name = "Customer 2"},
-                new Customer() {Name = "Customer 3"},
-                new Customer() {Name = "Customer 4"},
-                new Customer() {Name = "Customer 5"},
-                new Customer() {Name = "Customer 6"}
-            };
+            var movies = GetMovies();
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
+            return View(movies);
+        }
 
-            return View(viewModel);
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
